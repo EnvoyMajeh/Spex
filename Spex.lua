be0432c1902ecc61afb824240ff48d24a769ce46
EnvoyMajeh <41244825+EnvoyMajeh@users.noreply.github.com>
2018-07-20 22:53:26 -0400

    Cleaned up unused code, added comments
    removed old commented out code from CreateDropDown() and added some 
    comments for new maketext()

--- Spex.lua
+++ Spex.lua
@@ -15,29 +15,18 @@ local function makeText(specID)
 	HEALER	= "|TInterface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES:20:20:0:0:64:64:20:39:0:19|t",
 	DAMAGER	= "|TInterface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES:20:20:0:0:64:64:20:39:21:40|t",
 	NONE	= "|TInterface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES:20:20:0:0:64:64:20:39:21:40|t"
-	} -- end of role_icon{}
+  } -- end of role_icon{} --
   local _, name, _, icon, role, _ = GetSpecializationInfo(specID)
   return role_icon[role] .. name .. "|T" .. icon .. ":0|t"
-end -- end of makeText(specID)
+end -- end of makeText(specID) --
 
 -- Creates drop down menu entries based on player specializations --
 local function CreateDropDown(self, level)
---[[  local role_icon = {
-	-- Table of role icons for use in creating the text line. --
-	TANK	= "|TInterface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES:20:20:0:0:64:64:0:19:21:40|t",
-	HEALER	= "|TInterface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES:20:20:0:0:64:64:20:39:0:19|t",
-	DAMAGER	= "|TInterface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES:20:20:0:0:64:64:20:39:21:40|t",
-	NONE	= "|TInterface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES:20:20:0:0:64:64:20:39:21:40|t"
-	} -- end of role_icon{}
-]]--
 	-- Create drop down item for each spec --
   for i = 1, GetNumSpecializations() do
     local item = UIDropDownMenu_CreateInfo()
-    --local _, name, _, icon, role, _ = GetSpecializationInfo(i)
-    -- print(makeText(i))
-    item.text = makeText(i) --role_icon[role] .. name .. "|T" .. icon .. ":0|t"
+    item.text = makeText(i)
     item.value = i
-
     item.func = function(self)
 			-- clicking switches to selected spec, if not already in it --
       if GetSpecialization() ~= i then
@@ -52,7 +41,6 @@ end -- end of CreateDropDown(self, level) --
 local function Update()
   UIDropDownMenu_SetSelectedID(SpexFrame, GetSpecialization())
   UIDropDownMenu_SetText(SpexFrame, makeText(GetSpecialization()))
-
 end -- end of Update() --
 
 -- sets initial frame, dropdown and event settings --
@@ -68,6 +56,8 @@ local function init()
   else	-- else of if settings.Xpos --
     SpexFrame:SetPoint("CENTER", 0, 0)
   end -- end of if settings.Xpos --
+
+  -- default frame settings --
   SpexFrame:SetAlpha(0.75)
   SpexFrame:SetMovable(true)
   SpexFrame:EnableMouse(true)
@@ -76,6 +66,7 @@ local function init()
   SpexFrame:SetScript("OnDragStart", SpexFrame.StartMoving)
   SpexFrame:SetScript("OnDragStop", function(self)
     SpexFrame:StopMovingOrSizing()
+    -- saving frame location to settings --
     settings.XPos = SpexFrame:GetLeft()
     settings.YPos = SpexFrame:GetBottom()
   end) -- end of function(self)/SpexFrame:SetScript --
